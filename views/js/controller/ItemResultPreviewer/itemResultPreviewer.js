7c4439d683162a2d45a4d23d1b95408183bfbbac
andrii.ponamarov <ponamarevandrej@gmail.com>
2020-01-22 16:22:13 +0100

    Update bundle config

--- views/js/controller/ItemResultPreviewer/itemResultPreviewer.js
+++ views/js/controller/ItemResultPreviewer/itemResultPreviewer.js
@@ -1,33 +1,31 @@
 /**
  * @author Moyon Camille <camille@taotesting.com>
  */
-define([
-    'taoItems/previewer/factory'
-], function (previewerFactory) {
-    'use strict';
+define(["taoItems/previewer/factory"], function(previewerFactory) {
+  "use strict";
 
-    return {
-        start : function(config) {
-            var uri = config.uri;
-            var resultIdentifier = config.resultIdentifier;
-            var itemDefinition = config.itemDefinition;
-            var deliveryUri = config.deliveryIdentifier;
+  return {
+    start: function(config) {
+      var uri = config.uri;
+      var resultIdentifier = config.resultIdentifier;
+      var itemDefinition = config.itemDefinition;
+      var deliveryUri = config.deliveryIdentifier;
 
-            var uri = {
-                uri: uri,
-                resultId: resultIdentifier,
-                itemDefinition: itemDefinition,
-                deliveryUri: deliveryUri
-            };
+      var uri = {
+        uri: uri,
+        resultId: resultIdentifier,
+        itemDefinition: itemDefinition,
+        deliveryUri: deliveryUri
+      };
 
-            var type = config.type;
-            var state = config.state;
+      var type = config.type;
+      var state = config.state;
 
-            previewerFactory(type, uri, state, {
-                readOnly: true,
-                fullPage: true,
-                hideActionBars: true,
-            });
-        }
-    };
+      previewerFactory(type, uri, state, {
+        readOnly: true,
+        fullPage: true,
+        hideActionBars: true
+      });
+    }
+  };
 });
--- views/templates/previewer/index.tpl
+++ views/templates/previewer/index.tpl
@@ -7,8 +7,7 @@ use oat\tao\helpers\Layout;
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title><?=__('Item Result Previewer');?></title>
-        <?= \tao_helpers_Scriptloader::render() ?>
-        <?= Layout::getAmdLoader(Template::js('loader/app.min.js', 'tao'), 'ltiOutcomeUi/controller/ItemResultPreviewer/itemResultPreviewer', get_data('data')) ?>
+        <?= Layout::getAmdLoader(Template::js('loader/ltiOutcomeUi.min.js', 'ltiOutcomeUi'), 'ltiOutcomeUi/controller/ItemResultPreviewer/itemResultPreviewer', get_data('data')) ?>
     </head>
     <body></body>
 </html>
